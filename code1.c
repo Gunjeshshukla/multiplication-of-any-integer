f4b62496c85cbef250f6f67028138f852a13e0d7
Gunjeshshukla <gunjeshshukla56@gmail.com>
2025-08-24 08:07:07 +0530

    Add files via upload

---
+++ code1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int a;
+
+    printf("Enter a number :");
+    scanf("%d", &a);
+
+for( int i=0; i<=10; i++)
+printf("%d x %d = %d\n", a , i , a*i);
+    return 0;
+}
